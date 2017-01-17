d985f84816d6223ca75404192e9d77f3d7bf9df2
Ben Toussi <btoussi@central1.com>
2017-01-16 19:21:58 -0800

    Initial folder structure and linking materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app-component/app.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+import { RecordCardComponent } from './components/record-card/record-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    InstructionsComponent,
+    ActivitiesComponent,
+    RecordCardComponent
   ],
   imports: [
     BrowserModule,

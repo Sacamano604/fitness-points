fd0df866176e74b910a4941e6e2621f17ca482c6
Ben Toussi <btoussi@central1.com>
2017-01-18 19:51:41 -0800

    General form functionality

--- src/app/components/record-card/record-card.component.ts
+++ src/app/components/record-card/record-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordCardComponent implements OnInit {
   private activityList = [
-    { id: 1, name: 'A - Attendance' },
+    { id: 1, name: 'A - Attendance (Per Quarter)' },
     { id: 2, name: 'F - Fitness' },
     { id: 3, name: 'BD - Blood Donation' },
     { id: 4, name: 'ER - Environmental Responsibility' },
@@ -22,14 +22,42 @@ export class RecordCardComponent implements OnInit {
     { id: 13, name: 'WL12 - Weight Loss (Maintained 12 Months)' }
   ];
 
-  private activitySelect: number = 0;
-  // private log: string ='';
+  private fitnessList = [
+    { id: 1, name: 'Aerobics', pointsPerTime: 1, timePerPoints: 15 },
+    { id: 2, name: 'Badminton', pointsPerTime: 1, timePerPoints: 20 },
+    // Missing baseball softball here...
+    { id: 3, name: 'Basketball', pointsPerTime: 1, timePerPoints: 12 }
+  ];
 
-  private logDropdown(id: number): void {
-    const selectedName = this.activityList.find( (item: any) => item.id == id ).name;
-    // this.log += `Value ${NAME} was selected\n`;
-    console.log({ selectedName });
+  private maxPoints = [ 7, 8, 9, 10, 11, 12, 13]; // Values that award 200 points
+  private i: number = 0;
+  private pointsValue: number = 0;
+  private durationNUmber: number = 0;
+  private durationString: string = '';
 
+  private activityDropdown(id: number): void {
+    // Attendance
+    if (id == 1) {
+      this.pointsValue = 25;
+    }
+    // Fitness
+
+    // Blood Donation
+    if (id == 3) {
+      this.pointsValue = 10;
+    }
+
+    for (this.i = 0; this.i <= this.maxPoints.length; this.i++){
+      if (id == this.maxPoints[this.i]) {
+        this.pointsValue = 200;
+      }
+    }
+  }
+
+  private fitnessDropdown(id: number): void {
+    this.durationNUmber = this.fitnessList.find( (item: any) => item.id == id ).timePerPoints;
+    this.pointsValue = this.fitnessList.find( (item: any) => item.id == id ).pointsPerTime;
+    this.durationString = (this.durationNUmber + ' mins');
   }
 
   constructor() { }

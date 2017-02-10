b0d76fe1fedec73274bc15f77f36e026396cc7cd
Ben Toussi <btoussi@central1.com>
2017-02-10 08:51:04 -0800

    Back to firebase

--- src/app/components/record-card/add-record/add-record.component.ts
+++ src/app/components/record-card/add-record/add-record.component.ts
@@ -26,6 +26,7 @@ export class AddRecordComponent {
   }
 
   // Setting private variables that we will need for calculations and data binding.
+  private category: string;
   private maxPoints = [ 6, 7, 8, 9, 10, 11, 12]; // IDs that award 200 points
   private pointsValue: number;
   private originalPointsValue: number;
@@ -35,62 +36,63 @@ export class AddRecordComponent {
   private activityName: string;
 
   // Assigning points values if there is no calculations to be done.
-  private activityDropdown(id) {
-    // For some reason my ID comes through as a string here...
-    const idNumber = parseInt(id);    
-    switch(idNumber){
+  private activityDropdown(activityDropdownValue) {
+    const index = activityDropdownValue.split(' ')[0];
+    this.category = activityDropdownValue.split(' ')[1];
+
+    switch(this.category){
       // Attendance
-      case 0:
+      case 'attendance':
         this.pointsValue = 25;
       break;
-
       // Blood Donation
-      case 2:
+      case 'blooddonation':
        this.pointsValue = 10;
       break;
-
       // Environmental Responsibility
-      case 3:
+      case 'environmentresp':
         this.pointsValue = 15;
+      break;
+      // Quit Smoking
+      case 'quitsmoking':
+        this.pointsValue = 200;
+      break;
+      // Weight Loss
+      case 'weightloss':
+        this.pointsValue = 200;
       break;
       
       default:
     }
-
-    // These are the activities in the 'maxPoints' array that award the maximum points: 200.
-    const idAsNumber = parseInt(id);
-    for (let i = 0; i <= this.maxPoints.length; i++) {
-      if (idAsNumber === this.maxPoints[i]) {
-        this.pointsValue = 200;
-      }
-    }
-
   }
   
   // Finding and assigning the points value for fitness activity
-  private fitnessDropdown(id: number): void {
+  private fitnessDropdown(fitnessDropdownValue) {
+    const index = fitnessDropdownValue.split(' ')[0];
     this.fitnessList.subscribe( item => { 
      // Assigning the original values to use later in the increase/decrease functions
-     this.originalDurationNumber = item[id].timePerUnit;
-     this.originalPointsValue = item[id].pointsPerUnit;
+     this.originalDurationNumber = item[index].timePerUnit;
+     this.originalPointsValue = item[index].pointsPerUnit;
      // Assigning the values to use in the template
      this.pointsValue = this.originalPointsValue;
      this.durationNumber = this.originalDurationNumber;
-     this.unitofMeasurement = item[id].UnitofMeasurement;
+     this.unitofMeasurement = item[index].UnitofMeasurement;
     });
   }
 
   // Finding and assigning the points value for personal development activity
-  private personalDevDropDown(id: number): void {
+  private personalDevDropDown(personalDropdownValue) {
+    const index = personalDropdownValue.split(' ')[0];
     this.personalDevList.subscribe( item => {
-      this.pointsValue = item[id].points;
+      this.pointsValue = item[index].points;
     });
   }
 
   // Finding and assigning the points value for volunteer activities 
-  private volunteerDropDown(id: number): void {
+  private volunteerDropDown(volunteerDropDownValue) {
+    const index = volunteerDropDownValue.split(' ')[0];
     this.volunteeringList.subscribe( item => {
-      this.pointsValue = item[id].points;
+      this.pointsValue = item[index].points;
     });
   }
 
@@ -107,18 +109,7 @@ export class AddRecordComponent {
   }
 
   addRecord(activityDate: string, activity: string, fitnessActivity: string, personalActivity: string, volunteerActivity: string, duration: string, pointsEarned: number, notes: string): void {
-    var activityName = [];
-
-
-
-    this.activityList.subscribe( (activityList) => {
-      activityList.forEach(element => {
-        if (element.id === parseInt(activity)) {
-          activityName.push(element.name);
-          }
-      });
-    });
-    console.log(activityName);
+    console.log(activity);
 
 
     // this.activityRecord.push({ activityDate: activityDate, activity: test, fitnessActivity: fitnessActivity, personalActivity: personalActivity, volunteerActivity: volunteerActivity, duration: duration, pointsEarned: pointsEarned, notes: notes });

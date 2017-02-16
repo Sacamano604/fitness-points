855eab1d397b9856544b71e811441083fdc6aa5f
Ben Toussi <btoussi@central1.com>
2017-02-16 11:00:47 -0800

    Delete component working correctly

--- src/app/components/record-card/delete-record/delete-record.component.ts
+++ src/app/components/record-card/delete-record/delete-record.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'delete-record',
   templateUrl: './delete-record.component.html',
   styleUrls: ['./delete-record.component.css']
 })
+
 export class DeleteRecordComponent {
+  @Input() passedKey: string;
+  records: FirebaseListObservable<any>;
 
-  constructor() { }
+  constructor(af: AngularFire) {
+    this.records = af.database.list('/activityRecord');
+   }
 
-
-
-
-
+  deleteRecord(key: string) {
+    this.records.remove(key);
+  }
 }
--- src/app/components/record-card/record-card.component.ts
+++ src/app/components/record-card/record-card.component.ts
@@ -12,10 +12,8 @@ export class RecordCardComponent {
   @Input() activity: string;
   
   records: FirebaseListObservable<any>;
-  // activityList: FirebaseListObservable<any>;
 
   constructor(af: AngularFire) {
     this.records = af.database.list('/activityRecord');
-    // this.activityList = af.database.list('/masterList/activityList');
   }
 }

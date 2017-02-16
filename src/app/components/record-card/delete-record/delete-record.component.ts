733aa9170db51a72b966a65f882873d074867f02
Ben Toussi <btoussi@central1.com>
2017-02-16 09:49:52 -0800

    Moved delete button to it's own template

--- src/app/components/record-card/delete-record/delete-record.component.ts
+++ src/app/components/record-card/delete-record/delete-record.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-delete-record',
+  selector: 'delete-record',
   templateUrl: './delete-record.component.html',
   styleUrls: ['./delete-record.component.css']
 })
-export class DeleteRecordComponent implements OnInit {
+export class DeleteRecordComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
+
+
 
 }
